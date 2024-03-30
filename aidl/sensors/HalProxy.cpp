645aef779b4046c0215c85f4ed57a05b92d857e8
Georg Veichtlbauer <georg@vware.at>
2024-03-30 14:11:43 +0530

    sensors: Re-implement ALS capture as AIDL service
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- aidl/sensors/HalProxy.cpp
+++ aidl/sensors/HalProxy.cpp
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#include "AlsCorrection.h"
+
 #include "HalProxy.h"
 
 #include <android/hardware/sensors/2.0/types.h>
@@ -515,6 +517,11 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
+                    if (static_cast<int>(sensor.type) == SENSOR_TYPE_QTI_WISE_LIGHT) {
+                        sensor.type = SensorType::LIGHT;
+                        ALOGV("Replaced QTI Light sensor with standard light sensor");
+                        AlsCorrection::init();
+                    }
                     bool keep = patchOplusPickupSensor(sensor);
                     if (!keep) {
                         continue;
@@ -682,12 +689,18 @@ void HalProxy::resetSharedWakelock() {
     mWakelockTimeoutResetTime = getTimeNow();
 }
 
-void HalProxy::postEventsToMessageQueue(const std::vector<Event>& events, size_t numWakeupEvents,
+void HalProxy::postEventsToMessageQueue(const std::vector<Event>& eventsList, size_t numWakeupEvents,
                                         V2_0::implementation::ScopedWakelock wakelock) {
     size_t numToWrite = 0;
     std::lock_guard<std::mutex> lock(mEventQueueWriteMutex);
     if (wakelock.isLocked()) {
         incrementRefCountAndMaybeAcquireWakelock(numWakeupEvents);
+    }
+   std::vector<Event> events(eventsList);
+    for (auto& event : events) {
+        if (static_cast<int>(event.sensorType) == SENSOR_TYPE_QTI_WISE_LIGHT) {
+            AlsCorrection::correct(event.u.scalar);
+        }
     }
     if (mPendingWriteEventsQueue.empty()) {
         numToWrite = std::min(events.size(), mEventQueue->availableToWrite());
